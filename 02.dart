7609c2b1cfe61f6aab25346691d655472978a3f2
banjo254 <benjaminmariga@gmail.com>
2023-02-01 06:27:48 +0300

    return a funtion

---
+++ 02.dart
@@ -0,0 +1,9 @@
+void main() {
+  int cubeArea = findArea(5, 7, 9);
+  print(cubeArea);
+}
+
+int findArea(int length, int width, int height) {
+  int area = length * width * height;
+  return area;
+}
