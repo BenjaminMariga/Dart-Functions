c08730f15552f0d834685694d8c39b30862dac36
banjo254 <benjaminmariga@gmail.com>
2023-02-02 22:51:25 +0300

    parametrs optional 2

--- func.dart
+++ func.dart
@@ -1,6 +1,8 @@
 void main() {
   String Fnames = ny("James", "Bond");
+  String Prof = ny("Johana", "Whee", "PROF");
   print(Fnames);
+  print(Prof);
 }
 
 String ny(String fName, String lName, [String? title]) {
